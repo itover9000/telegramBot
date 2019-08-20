209370ec4a91e90ba84a0e7f3688b67a3c30884b
YuryP <over9000@tut.by>
2019-08-20 14:45:22 +0300

    added travis CI

--- src/main/test/java/com/util/YandexTranslateUtilTest.java
+++ src/main/test/java/com/util/YandexTranslateUtilTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,6 +21,6 @@ public class YandexTranslateUtilTest {
     @Test
     public void translateFromEnToRu() throws IOException {
         String helloWorld = yandexTranslateUtil.translateFromEngToRu("Hello world");
-        assertEquals("Привет мир", helloWorld);
+        assertEquals("Привет мир", new String(String.valueOf(helloWorld).getBytes(), StandardCharsets.UTF_8));
     }
 }
